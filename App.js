75d4f40799f293af8c81807e9c697cd0f2237864
Sahaj Singh Sohal <sahaj@geekyants.com>
2023-03-28 15:18:38 +0530

    ping-pong-game

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ export default function App() {
     <NativeBaseProvider>
       <GestureHandlerRootView>
         <Game />
-        {/* <Reanimated /> */}
       </GestureHandlerRootView>
     </NativeBaseProvider>
   );
